c53d33dec10c295dde088cc99a971c51a9ff8e4b
vinay manyam <vinayip541@gmail.com>
2017-10-17 09:23:56 +0530

    Add files via upload

---
+++ GridView/main/java/ml/projects2017/gridview/MainActivity.java
@@ -0,0 +1,13 @@
+package ml.projects2017.gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
